b46c269b4274788ec06b47970aa8783111752f73
Manpreet Singh <msbsoft@users.noreply.github.com>
2023-02-22 14:19:04 +0800

    wire image onload event

--- src/components/progressive-image/progressive-image.tsx
+++ src/components/progressive-image/progressive-image.tsx
@@ -1,32 +1,38 @@
-import {useState, useEffect} from "react";
-import * as styles from "./progressive-image.module.css"
+import { useState, useEffect } from "react";
+import * as styles from "./progressive-image.module.css";
 
 export type ProgressiveImageProps = {
-    placeholderImageSrc:string,
-    imageSrc:string,
-    [x:string]:string
-}
+  placeholderImageSrc: string;
+  imageSrc: string;
+  [x: string]: string;
+};
 
-const ProgressiveImage = ({placeholderImageSrc, imageSrc, ...props}:ProgressiveImageProps) => {
-    const [src, setSrc] = useState(placeholderImageSrc || imageSrc);
-    const [isLoading, setIsLoading] = useState(true);
-    
-    useEffect(() => {
-        const img = new Image();
-        img.src = imageSrc;
-        setTimeout(() => {
-            setSrc(_ => imageSrc);
-            setIsLoading(false);
-        }, 4000);
-    },[imageSrc])
-    
-    return (
-        <img
-            src={src}
-            className={isLoading ? styles.loading : styles.loaded}
-            {...props}          
-        />
-    )
-}
+const ProgressiveImage = ({
+  placeholderImageSrc,
+  imageSrc,
+  ...props
+}: ProgressiveImageProps) => {
+  const [src, setSrc] = useState(placeholderImageSrc || imageSrc);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = imageSrc;
+    img.onload = () => {
+      setTimeout(() => {
+        setSrc((_) => imageSrc);
+        setIsLoading(false);
+      }, 2000);
+    };
+  }, [imageSrc]);
+
+  return (
+    <img
+      src={src}
+      className={isLoading ? styles.loading : styles.loaded}
+      {...props}
+    />
+  );
+};
 
 export default ProgressiveImage;
